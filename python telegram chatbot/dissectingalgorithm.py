f725cebfdd970fc8372bdaed71986e0bab6f9041
RextealHD <deportiblog1@gmail.com>
2018-12-20 17:32:38 +0000

    Made changes to the algorithm

---
+++ python telegram chatbot/dissectingalgorithm.py
@@ -0,0 +1,50 @@
+x = 0 #variable for a loop
+i = 0 #variable for a loop
+text = input("Enter a message: ") #String input
+newtext = list(text) #creates a list with all the characters in the string
+lmessage = len(newtext) #counts the characters in the message
+print(newtext)
+print(lmessage)
+for i in range(lmessage): #loop to go through all the characters in the list
+    #This code deletes all the punctuation characters in the list
+    if "," in newtext:
+        newtext.remove(",")
+    elif "." in newtext:
+        newtext.remove(".")
+    elif ":" in newtext:
+        newtext.remove(":")
+    elif "!" in newtext:
+        newtext.remove("!")
+    elif "?" in newtext:
+        newtext.remove("?")
+    elif "(" in newtext:
+        newtext.remove("(")
+    elif ")" in newtext:
+        newtext.remove(")")
+    elif "/" in newtext:
+        newtext.remove("/")
+    elif "'" in newtext:
+        newtext.remove("'")
+    elif "[" in newtext:
+        newtext.remove("[")
+    elif "]" in newtext:
+        newtext.remove("]")
+    elif "=" in newtext:
+        newtext.remove("=")
+    elif "+" in newtext:
+        newtext.remove("+")
+    elif "£" in newtext:
+        newtext.remove("£")
+    elif "$" in newtext:
+        newtext.remove("$")
+    elif "*" in newtext:
+        newtext.remove("*")
+    elif ";" in newtext:
+        newtext.remove(";")
+print(newtext)
+solvedtext = ''.join(newtext) #joins the list to create a string
+print(solvedtext)
+word = solvedtext.split(' ')#Splits the string at every space space
+lword = len(word) #Counts the words within the list
+for x in range(lword):
+    print(word[x]) #prints all the words in the sentence
