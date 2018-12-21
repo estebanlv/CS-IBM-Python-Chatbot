d990b7c4468f79e5e0da2b36ebb2dddab0ce36d7
RextealHD <deportiblog1@gmail.com>
2018-12-21 21:56:58 +0000

    Created word databes. Made small changes to textfileanalyser

--- python telegram chatbot/textfileanalyser.py
+++ python telegram chatbot/textfileanalyser.py
@@ -1,5 +1,11 @@
-print("This program will read anything on a text file and save the words into a database")
+from openpyxl import load_workbook #This loads a library that allows me to read/write excel files
+wb = load_workbook(filename = 'wordsdatabase.xlsx') #Tell the program what file i'm using
+sheet = wb['main'] #Tells the program which sheet in the document i'm using
+letters = ["A","B","C","D","E","F","G","H","I","K","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+#These variable above is for cell values
 
+
+print("This program will read anything on a text file and save the words into a database")
 
 text_file = open("texttoread.txt", "r") #opens the text file to read it
 lines = text_file.readlines() #reads every line in the file and stores it in a list
